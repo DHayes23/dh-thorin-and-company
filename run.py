5d272b28dec2e0315bc8b488f481d67a646d23c5
DHayes23 <d.hayesdeveloper@gmail.com>
2021-11-02 22:40:07 +0000

    Swapped all files with CI files

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 
@@ -18,7 +19,8 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company = data)
+    return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -28,10 +30,10 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    return render_template("member.html", member = member)
+    return render_template("member.html", member=member)
 
 
-@app.route("/contact", methods = ["GET", "POST"])
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         flash("Thanks {}, we have received your message!".format(
